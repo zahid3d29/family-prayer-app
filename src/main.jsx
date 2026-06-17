9d8d5c9e0a2882ea5c25427fb0d66fc53a58897d
zahid3d29 <zahid3d29@gmail.com>
2026-06-17 19:29:48 +0600

    Add GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </React.StrictMode>,
-)
+  </HashRouter>
+);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
+
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  base: "/family-prayer-app/",
+  plugins: [react()],
+});
+
+// export default defineConfig({
+//   base: "/family-prayer-app/",
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+// })
+
 
 
 // import { defineConfig } from 'vite'
